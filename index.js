234df0cb5c8e5ac232dca34813f3d15099e20499
koichiro12 <peengine889@gmail.com>
2024-09-20 11:26:52 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const { createProxyMiddleware }  = require('http-proxy-middleware')
+
+const port = 3000
+
+//Contoh simple Proxy middleware for API
+// microservice api bisa anda buat menggunakan
+app.use('/api',createProxyMiddleware({
+    target:'http://localhost:3001',
+    changeOrigin:true,
+    logger:console,
+    pathRewrite:{
+        '^/api' : ''
+    }
+}));
+
+//Blok semua request ke url http://localhost:3000/
+app.all('/',(req,res)=>{
+    res.status(403).send("Sori ngab, Gaboleh Masuk :D")
+})
+app.listen(port,()=>{
+    console.log("API Gateway Listening at http://localhost:"+port);
+})
